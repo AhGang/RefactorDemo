93aa781252bb1aed1c90de777e2b499680972223
AhGang <474494594@qq.com>
2019-08-12 20:15:39 +0800

    [Refactor] use switch cases to updateQuality because the code Logical confusion

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -12,41 +12,57 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (isNameNotEqualAGED_BRIEOrBACKSTAGE(items[i])) {
-                if (items[i].quality > 0) {
-                    if (isNameNotEqualSULFURAS(items[i], SULFURAS_HAND_OF_RAGNAROS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                    if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        addQualityWhenNameEqualsBACKSTAGE(items[i]);
-                    }
-                }
-            }
-
-            if (isNameNotEqualSULFURAS(items[i], SULFURAS_HAND_OF_RAGNAROS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!isNameNotEqualAGED(items[i], AGED_BRIE)) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                } else if (!isNameNotEqualBACKSTAGE(items[i], BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    items[i].quality = 0;
-                } else {
-                    if (items[i].quality > 0) {
-                        if (isNameNotEqualSULFURAS(items[i], SULFURAS_HAND_OF_RAGNAROS)) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
+            switch (items[i].name) {
+                case AGED_BRIE:
+                    updateAGED_BRIEQuality(items[i]);
+                    break;
+                case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
+                    updateBACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERTQuality(items[i]);
+                    break;
+                case SULFURAS_HAND_OF_RAGNAROS:
+                    updateSULFURAS_HAND_OF_RAGNAROSQuality(items[i]);
+                    break;
+                default:
+                    updateOthersQuality(items[i]);
             }
         }
+    }
+
+    private void updateOthersQuality(Item item) {
+        if (item.quality > 0) {
+            if (item.sellIn < 0) {
+                item.quality -= 1;
+            }
+            item.quality -= 1;
+        }
+        item.sellIn -= 1;
+    }
+
+    private void updateSULFURAS_HAND_OF_RAGNAROSQuality(Item item) {
+    }
+
+    private void updateBACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERTQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                addQualityWhenNameEqualsBACKSTAGE(item);
+            }
+        }
+        item.sellIn -= 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void updateAGED_BRIEQuality(Item item) {
+        if (item.quality < 50) {
+            if (item.sellIn < 0) {
+                item.quality += 1;
+            }
+            item.quality += 1;
+        }
+        item.sellIn -= 1;
     }
 
     private boolean isNameNotEqualBACKSTAGE(Item item, String backstagePassesToATafkal80EtcConcert) {
